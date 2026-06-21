df8a12db1b065a7dcdacd6b57a620fdd7d49494e
unknown <mangulabnanshervinmarco@gmail.com>
2026-06-21 21:26:16 +0800

    Add model relationships

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'type',
+        'amount',
+        'description',
+        'transaction_date'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
